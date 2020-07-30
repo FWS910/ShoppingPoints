2e9fb483259d2b5db62b14864b8480ff233ea576
fws06118 <1241786550@qq.com>
2020-07-30 21:26:16 +0800

    [fws]特价商品积分超过1000测试

--- src/test/java/com/thoughtworks/basic/CalculatePointTest.java
+++ src/test/java/com/thoughtworks/basic/CalculatePointTest.java
@@ -15,8 +15,8 @@ public class CalculatePointTest {
         //given
         Calculateintegral calculatePoint = new Calculateintegral();
         ArrayList<Goods> goodsList = new ArrayList<Goods>();
-        Goods cucumber = new Goods("tomato", new BigDecimal(50));
-        Goods orange = new Goods("watermelon", new BigDecimal(70));
+        Goods cucumber = new Goods("TOMATO", new BigDecimal(50));
+        Goods orange = new Goods("WATERMELON", new BigDecimal(70));
         goodsList.add(cucumber);
         goodsList.add(orange);
         //when
@@ -26,21 +26,36 @@ public class CalculatePointTest {
     }
 // 需求二
     @Test
-    public void should_return_120_point_when_calculate_given_include_promotions_goods_and_amount_is_100() {
+    public void should_return_190_point_when_calculate_given_include_promotions_goods_and_amount_is_100() {
         //given
         Calculateintegral calculatePoint = new Calculateintegral();
         ArrayList<Goods> goodsList = new ArrayList<Goods>();
-        Goods WATERMELON = new Goods("watermelon", new BigDecimal(70));
+        Goods WATERMELON = new Goods("WATERMELON", new BigDecimal(70));
         Goods APPLE = new Goods("APPLE", new BigDecimal(20));
-        Goods Detergent = new Goods("Tomato", new BigDecimal(10));
+        Goods Detergent = new Goods("TOMATO", new BigDecimal(10));
         goodsList.add(WATERMELON);
         goodsList.add(APPLE);
         goodsList.add(Detergent);
         //when
         int totalPoint = calculatePoint.calculate(goodsList);
         //then
-        Assert.assertEquals(totalPoint, 120);
+        Assert.assertEquals(totalPoint, 190);
     }
+    //    需求三
+    @Test
+    public void should_return_1067_point_when_calculate_given_amount_is_2350() {
+        //given
+        Calculateintegral calculatePoint = new Calculateintegral();
+        ArrayList<Goods> goodsList = new ArrayList<Goods>();
+        Goods WATERMELON = new Goods("REFRIGERATOR", new BigDecimal(2350));
+        goodsList.add(WATERMELON);
+        //when
+        int totalPoint = calculatePoint.calculate(goodsList);
+        //then
+        Assert.assertEquals(totalPoint, 1067);
+    }
+
+
 
 
 
